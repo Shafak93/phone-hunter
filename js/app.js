4c059e3dad5d9350d347609563f29cf50e2711b4
Shafak93 <shafakctg64@gmail.com>
2022-03-01 16:08:43 +0600

    comment written

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//serach box data load
 const searchPhone = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value
@@ -17,9 +18,8 @@ const searchPhone = () =>{
     }
     
 }
-
+//Search result data
 const displayPhone = phones =>{
-    console.log(phones);
     const phoneCardDiv = document.getElementById('phone-card');
     phoneCardDiv.textContent = ''
     if(phones.length == 0 ){
@@ -34,12 +34,15 @@ const displayPhone = phones =>{
             const div = document.createElement('div');
             div.classList.add('col');
         div.innerHTML = `
-                <div class="card p-4 rounded">
+                <div class="h-100 card p-4 rounded">
                     <img src="${phone.image}" class="card-img-top" alt="...">
                     <div class="card-body text-center">
                       <h5 class="card-title">${phone.phone_name}</h5>
                       <p>${phone.brand}</p>
-                      <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
+                      
+                    </div>
+                    <div class="card-footer text-center">
+                    <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
                     </div>
                   </div>
         `
@@ -50,8 +53,8 @@ const displayPhone = phones =>{
     
 
 }
+//Button details Data load
 const loadPhoneDetails = slug =>{
-    console.log(slug);
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`
     fetch(url)
     .then(res => res.json())
@@ -59,9 +62,8 @@ const loadPhoneDetails = slug =>{
 
 }
 
-
+//Button details card result
 const displayPhoneDetails = details =>{
-    console.log(details)
     const cardDetailDiv = document.getElementById('card-detail');
     cardDetailDiv.textContent =''
     const div = document.createElement('div');
@@ -71,14 +73,14 @@ const displayPhoneDetails = details =>{
     div.innerHTML =`
                   <div class="col-md-3">
                     <img src="${details.image}" class="img-fluid rounded-center w-100" alt="...">
-                    <div class='text-center mt-3'>
+                    <div class='text-center text-md-center mt-3'>
                         <h5 class="card-title fw-bolder">${details.brand}</h5>
                         <h6 class="card-title fw-bolder">${details.name}</h6>
                     </div>
                   </div>
                   <div class="col-md-9">
                     <div class="card-body">
-                      <dl class="row">
+                      <dl class="row text-wrap">
                         <dt class="col-sm-3">
                         Release date</dt>
                         <dd class="col-sm-9">${releaseDate}</dd>
@@ -98,7 +100,13 @@ const displayPhoneDetails = details =>{
                         <dd class="col-sm-9">${details.mainFeatures.memory}</dd>
                         
                         <dt class="col-sm-3 text-truncate">sensors</dt>
-                        <dd class="col-sm-9">${details.mainFeatures.sensors}</dd>
+                        <dd class="col-sm-9 text-wrap">${details.mainFeatures.sensors[0]},<br>
+                        ${details.mainFeatures.sensors[1]}<br>
+                        ${details.mainFeatures.sensors[2]}<br>
+                        ${details.mainFeatures.sensors[3]}<br>
+                        ${details.mainFeatures.sensors[4]}<br>
+                        ${details.mainFeatures.sensors[5]}
+                        </dd>
                         
                         <dt class="col-sm-3 text-truncate">Bluetooth</dt>
                         <dd class="col-sm-9">${details.others == undefined ? 'no bluetooth info found' : details.others.Bluetooth}</dd>
