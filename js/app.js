52dbb2a604666c0ae4d9c39bafd3096580f6f638
Shafak93 <shafakctg64@gmail.com>
2022-03-01 08:09:10 +0600

    single card function

--- js/app.js
+++ js/app.js
@@ -4,24 +4,24 @@ const searchPhone = () =>{
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     searchField.value = ''
-
+    
     if(searchText == ''){
         const noSearch = document.getElementById('no-search');
         noSearch.innerHTML = `
-        <p class="text-center text-danger">Please write phone name to show results</p>
+        <h3 class="text-center text-danger">Please write phone name to show results</h3>
         `
     }else{
         fetch(url)
         .then(res => res.json())
-        .then(data => displayPhone(data.data));
+        .then(data => displayPhone(data.data))
     }
     
 }
-searchPhone();
 
 const displayPhone = phones =>{
     console.log(phones);
     const phoneCardDiv = document.getElementById('phone-card');
+    phoneCardDiv.textContent = ''
     
     phones.forEach(phone =>{
         const div = document.createElement('div');
@@ -32,7 +32,7 @@ const displayPhone = phones =>{
                 <div class="card-body">
                   <h5 class="card-title">${phone.phone_name}</h5>
                   <p>${phone.brand}</p>
-                  <button onclick="loadPhoeDetails('${phone.slug}')" class="btn btn-primary">Details</button>
+                  <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
                 </div>
               </div>
     `
@@ -40,15 +40,45 @@ const displayPhone = phones =>{
     })
 
 }
-const loadPhoeDetails = slug =>{
+const loadPhoneDetails = slug =>{
     console.log(slug);
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data))
+    .then(data => displayPhoneDetails(data.data))
 
 }
 
-// const displayPhoneDetails = details =>{
 
-// }
+const displayPhoneDetails = details =>{
+    console.log(details)
+    const cardDetailDiv = document.getElementById('card-detail');
+    const div = document.createElement('div');
+    div.classList.add('row')
+    const releaseDate = details.releaseDate == ''? 'No release date found' : details.releaseDate
+    div.innerHTML =`
+            
+                  <div class="col-md-4">
+                    <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${details.brand}</h5>
+                      <h5 class="card-title">${details.name}</h5>
+                    <h5>Release date:</h5>${releaseDate}
+                    </div>
+                  </div>
+            
+    `
+    
+    
+  /*   `
+    <img src="${details.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${details.name}</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    `; */
+    cardDetailDiv.appendChild(div);
+
+}
